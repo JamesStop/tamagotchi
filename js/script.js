f38bc95feac3b7db8c1b79aba868cd76b57c4ea2
xelphite <stoppanij@gmail.com>
2022-05-23 14:44:00 -0700

    planning stuff

---
+++ js/script.js
@@ -0,0 +1,59 @@
+/*----- constants -----*/
+const BABY_IMAGE = './assets/baby.webp';
+const CHILD_IMAGE = './assets/child.webp';
+const TEEN_IMAGE = './assets/teen/webp';
+const ADULT_IMAGE = './assets/adult.webp';
+const MAX_STAT_VALUE = 100;
+const MAX_BABY_AGE = 10;
+const MAX_CHILD_AGE = 19;
+const MAX_TEEN_AGE = 29;
+const HUNGER_INTERVAL = 5000;
+const BOREDOM_INTERVAL = 6000;
+const AGE_INTERVALL = 60000;
+const SLEEPINESS_INTERVAL = 3000;
+
+/*----- app's state (variables) -----*/
+
+let age;
+let hunger;
+let boredom;
+let sleepiness;
+
+/*----- cached element references -----*/
+
+
+
+
+
+
+
+/*----- event listeners -----*/
+
+
+
+
+
+
+
+/*----- functions -----*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//End
